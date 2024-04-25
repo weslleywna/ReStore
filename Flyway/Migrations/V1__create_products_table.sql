427ba3d57de49ae2e6c574762e700f2dd99b12ad
Weslley <weslley_sjn@hotmail.com>
2024-04-24 21:01:53 -0300

    adjust script create table

--- Flyway/Migrations/V1__create_products_table.sql
+++ Flyway/Migrations/V1__create_products_table.sql
@@ -1,5 +1,7 @@
-CREATE TABLE products (
-    id UUID PRIMARY KEY,
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA public;
+
+CREATE TABLE IF NOT EXISTS products (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     brand VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     type VARCHAR(255) NOT NULL,
