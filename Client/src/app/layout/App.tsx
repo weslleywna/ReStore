e5e4dea5502eda54583f8775f5710ad0a7dc1c74
Weslley Nascimento <weslley_sjn@hotmail.com>
2024-05-20 19:52:11 -0300

    create ProductList, ProductCard, install materialUI, add prettier to format

---
+++ Client/src/app/layout/App.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { Product } from '../models/product';
+import Catalog from '../../features/catalog/Catalog';
+import { Typography } from '@mui/material';
+
+function App() {
+	const [products, setProducts] = useState<Product[]>([]);
+
+	useEffect(() => {
+		fetch('http://localhost:5000/api/products')
+			.then((response) => response.json())
+			.then((data) => setProducts(data));
+	}, []);
+
+	function addProduct() {
+		setProducts((prevState) => [
+			...prevState,
+			{
+				id: `${prevState.length + 1}`,
+				name: 'product' + (prevState.length + 1),
+				price: prevState.length * 100 + 100,
+				brand: 'some brand',
+				description: 'some description',
+				pictureUrl: 'http://picsum.photos/200',
+				quantityInStock: 10,
+				type: 'some type',
+			},
+		]);
+	}
+
+	return (
+		<div>
+			<Typography variant="h1">Re-Store</Typography>
+			<Catalog products={products} addProduct={addProduct} />
+		</div>
+	);
+}
+
+export default App;
